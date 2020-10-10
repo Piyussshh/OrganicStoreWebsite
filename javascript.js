489a0cbbb7967716d467515862f3b248a458aea9
Piyush <piyushkumargupta30@gmail.com>
2020-10-10 16:49:30 +0530

    intial commit

---
+++ javascript.js
@@ -0,0 +1,188 @@
+var home = document.getElementById("ahome");
+
+home.addEventListener("mouseover", function() {
+      var ul = document.querySelector("#ahome ul#home");
+      ul.style.display = "block";
+});
+
+home.addEventListener("mouseout", function() {
+      var ul = document.querySelector("#ahome ul#home");
+      ul.style.display = "none";
+});
+
+// ***************************************************************Store***************************************************************
+
+var store = document.getElementById("listore");
+
+store.addEventListener("mouseover", function() {
+      var ul = document.querySelector("#listore ul#store");
+      ul.style.display = "block";
+});
+
+store.addEventListener("mouseout", function() {
+       var ul = document.querySelector("#listore ul#store");
+      ul.style.display = "none";
+});
+
+// ***************************************************************Feature***************************************************************
+
+
+var feature = document.getElementById("lifeatures");
+
+feature.addEventListener("mouseover", function() {
+      var ul = document.querySelector("#lifeatures ul#features");
+      ul.style.display = "block";
+});
+
+feature.addEventListener("mouseout", function() {
+       var ul = document.querySelector("#lifeatures ul#features");
+      ul.style.display = "none";
+});
+
+// ***************************************************************News***************************************************************
+
+
+var news  =  document.querySelector("#linews");
+
+news.addEventListener("mouseover", function() {
+      var ul = document.querySelector("#linews ul#News");
+      ul.style.display = "block";
+});
+news.addEventListener("mouseout", function() {
+    var ul = document.querySelector("#linews ul#News");
+      ul.style.display = "none";
+});
+
+
+// ***************************************************************Gallery***************************************************************
+
+var gallery  =  document.querySelector("#liGallery");
+
+gallery.addEventListener("mouseover", function() {
+      var ul = document.querySelector("#liGallery ul#Gallery");
+      ul.style.display = "block";
+});
+gallery.addEventListener("mouseout", function() {
+    var ul = document.querySelector("#liGallery ul#Gallery");
+      ul.style.display = "none";
+});
+// ***************************************************************counter***************************************************************
+
+var heart = document.querySelector(".fa-heart");
+ heart.addEventListener("click",function(){
+ 	var text1 = document.querySelector("#count")
+    var counter = text1.textContent;
+    counter++;
+ 	text1.textContent = counter ;
+ 	
+ 
+ });
+
+// var heart = document.querySelector(".fa-heart");
+//  heart.addEventListener("click",function(){
+//  	var text1 = document.querySelector("#count1")
+//     var counter = text1.textContent;
+//     counter++;
+//  	text1.textContent = counter ;
+ 	
+ 
+//  });
+
+// var heart = document.querySelector(".fa-heart");
+//  heart.addEventListener("click",function(){
+//  	var text1 = document.querySelector("#count2")
+//     var counter = text1.textContent;
+//     counter++;
+//  	text1.textContent = counter ;
+ 	
+ 
+//  });
+
+// var heart = document.querySelector(".fa-heart");
+//  heart.addEventListener("click",function(){
+//  	var text1 = document.querySelector("#count3")
+//     var counter = text1.textContent;
+//     counter++;
+//  	text1.textContent = counter ;
+ 	
+ 
+//  });
+
+// var heart = document.querySelector(".fa-heart");
+//  heart.addEventListener("click",function(){
+//  	var text1 = document.querySelector("#count4")
+//     var counter = text1.textContent;
+//     counter++;
+//  	text1.textContent = counter ;
+ 	
+ 
+//  });
+
+// var heart = document.querySelector(".fa-heart");
+//  heart.addEventListener("click",function(){
+//  	var text1 = document.querySelector("#count5")
+//     var counter = text1.textContent;
+//     counter++;
+//  	text1.textContent = counter ;
+ 	
+ 
+//  });
+//  var heart = document.querySelector(".fa-heart");
+//  heart.addEventListener("click",function(){
+//  	var text1 = document.querySelector("#count6")
+//     var counter = text1.textContent;
+//     counter++;
+//  	text1.textContent = counter ;
+ 	
+ 
+//  });
+//  var heart = document.querySelector(".fa-heart");
+//  heart.addEventListener("click",function(){
+//  	var text1 = document.querySelector("#count7")
+//     var counter = text1.textContent;
+//     counter++;
+//  	text1.textContent = counter ;
+ 	
+ 
+//  });
+//  var heart = document.querySelector(".fa-heart");
+//  heart.addEventListener("click",function(){
+//  	var text1 = document.querySelector("#count8")
+//     var counter = text1.textContent;
+//     counter++;
+//  	text1.textContent = counter ;
+ 	
+ 
+//  });
+//  var heart = document.querySelector(".fa-heart");
+//  heart.addEventListener("click",function(){
+//  	var text1 = document.querySelector("#count9")
+//     var counter = text1.textContent;
+//     counter++;
+//  	text1.textContent = counter ;
+ 	
+ 
+//  });
+//  var heart = document.querySelector(".fa-heart");
+//  heart.addEventListener("click",function(){
+//  	var text1 = document.querySelector("#count10")
+//     var counter = text1.textContent;
+//     counter++;
+//  	text1.textContent = counter ;
+ 	
+ 
+//  });
+//  var heart = document.querySelector(".fa-heart");
+//  heart.addEventListener("click",function(){
+//  	var text1 = document.querySelector("#count")
+//     var counter = text1.textContent;
+//     counter++;
+//  	text1.textContent = counter ;
+ 	
+ 
+//  });
+
+
+
+
+
